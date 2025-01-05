b7cbf568473d55916dcd56c12edcc6af51a88168
JeongWook Lee <44370562+Ljwook92@users.noreply.github.com>
2025-01-04 19:31:41 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("ou6Ni_FKaZs8uTG7F");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_mgflzg8', '__ejs-test-mail-service__', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
