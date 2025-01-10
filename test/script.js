72d767bea3123ec0cfe2b83fe4befc2f5ff3e059
JeongWook Lee <44370562+Ljwook92@users.noreply.github.com>
2025-01-09 18:19:39 -0600

    Update script.js

--- test/script.js
+++ test/script.js
@@ -20,50 +20,50 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Personal Website | Jung-In Seo";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/favhand.png");
+            document.title = "Come Back To Personal Website";
+            $("#favicon").attr("href", "/assets/images/whitebear.jpg");
         }
     });
 
 
 // fetch projects start
-function getProjects() {
-    return fetch("projects.json")
-        .then(response => response.json())
-        .then(data => {
-            return data
-        });
-}
+// function getProjects() {
+//    return fetch("projects.json")
+//        .then(response => response.json())
+//        .then(data => {
+//            return data
+//        });
+//}
 
 
-function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
-    projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>`
-    });
-    projectsContainer.innerHTML = projectsHTML;
+//function showProjects(projects) {
+//    let projectsContainer = document.querySelector(".work .box-container");
+//    let projectsHTML = "";
+//    projects.forEach(project => {
+//        projectsHTML += `
+//        <div class="grid-item ${project.category}">
+//        <div class="box tilt" style="width: 380px; margin: 1rem">
+//      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+//      <div class="content">
+//        <div class="tag">
+//        <h3>${project.name}</h3>
+//        </div>
+//        <div class="desc">
+//          <p>${project.desc}</p>
+//          <div class="btns">
+//            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+//            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+//          </div>
+//        </div>
+//      </div>
+//    </div>
+//    </div>`
+//    });
+//    projectsContainer.innerHTML = projectsHTML;
 
     // vanilla tilt.js
     // VanillaTilt.init(document.querySelectorAll(".tilt"), {
@@ -83,6 +83,39 @@ function showProjects(projects) {
     // srtop.reveal('.work .box', { interval: 200 });
 
     // isotope filter products
+//    var $grid = $('.box-container').isotope({
+//        itemSelector: '.grid-item',
+//        layoutMode: 'fitRows',
+//        masonry: {
+//            columnWidth: 200
+//        }
+//    });
+
+    // filter items on button click
+//    $('.button-group').on('click', 'button', function () {
+//        $('.button-group').find('.is-checked').removeClass('is-checked');
+//        $(this).addClass('is-checked');
+//        var filterValue = $(this).attr('data-filter');
+//        $grid.isotope({ filter: filterValue });
+//    });
+//}
+
+function showPublications(publications) {
+    let publicationsContainer = document.querySelector(".publications-list .box-container");
+    let publicationsHTML = "";
+    publications.forEach(publication => {
+        publicationsHTML += `
+        <div class="grid-item ${publication.category}">
+        <div class="box" style="width: 380px; margin: 1rem">
+          <h3>${publication.name}</h3>
+          <p>${publication.desc}</p>
+          <a href="${publication.link}" class="btn" target="_blank">Read More</a>
+        </div>
+        </div>`;
+    });
+    publicationsContainer.innerHTML = publicationsHTML;
+
+    // isotope filter publications
     var $grid = $('.box-container').isotope({
         itemSelector: '.grid-item',
         layoutMode: 'fitRows',
@@ -99,6 +132,7 @@ function showProjects(projects) {
         $grid.isotope({ filter: filterValue });
     });
 }
+
 
 getProjects().then(data => {
     showProjects(data);
